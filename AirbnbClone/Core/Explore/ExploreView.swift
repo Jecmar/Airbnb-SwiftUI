f836c6a16c6a0bd911bfecf053b5523f74a4ed66
Josue Cruz <jcruz@globalsoft.email>
2024-07-07 11:48:57 -0600

    Add background color, add search bar component and restructured code

--- AirbnbClone/Core/Explore/ExploreView.swift
+++ AirbnbClone/Core/Explore/ExploreView.swift
@@ -10,14 +10,20 @@ import SwiftUI
 struct ExploreView: View {
     var body: some View {
         NavigationStack{
-            ScrollView{
-                LazyVStack(spacing: 22){
-                    ForEach(0 ... 10, id: \.self){ listening in
-                        ListingItemView()
+            VStack{
+                SearchAndFilteBar(displayFilter: .constant(true))
+                    .padding(.horizontal)
+                
+                ScrollView{
+                    LazyVStack(spacing: 22){
+                        ForEach(0 ... 10, id: \.self){ listening in
+                            ListingItemView()
+                        }
                     }
+                    .padding()
                 }
-                .padding()
             }
+            .background(Color("background"))
         }
     }
 }
